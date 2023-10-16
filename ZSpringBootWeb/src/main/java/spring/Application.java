d9a67b816751c2abba64f268fa8320ca94b3b86d
bitcamp <bitcamp@DESKTOP-05CBH8L>
2023-10-16 16:35:53 +0900

    20231016 : Create test repository

---
+++ ZSpringBootWeb/src/main/java/spring/Application.java
@@ -0,0 +1,17 @@
+package spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+ * 혹시 AOP 구동이 안된다면...
+ *  아래와 같이 AutoProxy 를 활성화 시키자.
+ */
+//@EnableAspectJAutoProxy
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
